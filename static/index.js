f9d46653ca44853417edb0e2232bbf160ff80507
zafarabdul <zafarabdul05@gmail.com>
2025-02-26 00:11:50 +0530

    certificates

--- static/index.js
+++ static/index.js
@@ -22,3 +22,22 @@ function change(a){
         ch.style.display="none";
     }
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".card, ul,h4"); // Select both cards and containers
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+            }
+            else{
+                entry.target.classList.remove("in-view");
+            }
+        });
+    }, { threshold: 0.5 }); // Trigger when 50% of the element is visible
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
